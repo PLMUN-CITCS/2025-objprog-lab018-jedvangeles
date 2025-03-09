ea7eae9901f987bae93d2dd9798dba974eed059f
James Edward Danielle V. Angeles <jedangeles4@gmail.com>
2025-03-09 13:49:40 +0800

    Submitting OBJPROG Week 05 - Laboratory #18

---
+++ EvenOddChecker.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class EvenOddChecker {
+    public static void main(String[] args) {
+        int number = getIntegerInput();
+        String result = checkEvenOrOdd(number);
+        System.out.println(result);
+    }
+
+    public static int getIntegerInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+        scanner.close();
+        return number;
+    }
+
+    public static String checkEvenOrOdd(int number) {
+        if (number % 2 == 0) {
+            return number + " is an Even number.";
+        } else {
+            return number + " is an Odd number.";
+        }
+    }
+}
+
+
